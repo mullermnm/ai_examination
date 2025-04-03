79be525676ddfc8109c0ad00426ca976cd49c83d
Mulugeta Gebrekirstos <151052977+mullermnm@users.noreply.github.com>
2025-04-03 15:48:42 +0300

    Update sms.js

--- backend/src/helpers/sms.js
+++ backend/src/helpers/sms.js
@@ -2,14 +2,6 @@ export const sendConfirmationCode = async function(phoneNumber, code) {
     try {
       // send confirmation code
       console.info({ phoneNumber, code });
-    //   const accountSid = 'ACcaf92b19df9a6728973e0a7ad148aa63';
-    //   const authToken = '1bebf3f0e7e38398bc13c75c83e073e9';
-    //   const client = require('twilio')(accountSid, authToken);
-
-    // client.verify.v2.services("VA27c551dec59f0b0944590fe19922f39d")
-    //   .verifications
-    //   .create({to: '+251948461673', channel: 'sms'})
-    //   .then(verification => console.log(verification.sid));
       
 
 
